a331094c5aa2b63d116176162d4ddd1770538858
Sumon-Amdad <sumonbd19821980@gmail.com>
2020-07-18 08:24:18 +0600

    first commit

---
+++ assignment.js
@@ -0,0 +1,34 @@
+// function name
+// feetToMile
+// woodCalculator
+// brickCalculator
+// tinyFriend
+
+
+// 1 No. function name (own thinking solution)
+
+function feetToMile(feet){
+	var mile = feet / 5280;
+	return mile;
+}
+
+// 2 No. function name (own thinking solution)
+
+function woodCalculator(chair,table,bed) {
+	
+	var chairWoodCubic = chair * 5 ;
+	var tableWoodCubic = table * 5 ;
+	var bedWoodCubic = bed * 2 ;
+	var totalWoodCubic = chairWoodCubic + tableWoodCubic + bedWoodCubic;
+	return totalWoodCubic;
+}
+
+// 3 No. function name (own thinking + google help solution but i am not satisfied)
+
+function tinyFriend(friends) {
+	var min = friends[0].length;
+	friends.map(v => min = Math.min(min, v.length));
+	smallestFriend = friends.filter(v => v.length == min);
+	return smallestFriend;
+  }
+  
